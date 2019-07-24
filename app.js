525f098e904dcd601102d8cfa136ac9d64a6b663
Dkilpatrick31 <dkilpatrick1990@gmail.com>
2019-07-24 02:10:14 -0500

    Created two props in for the header in the App component.

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
-const Header = () =>
+const Header = (props) => {
+  return (
     <header>
-      <h1>Scoreboard</h1>
-      <span className="stats">Players: 1</span>
-    </header>;
+      <h1>{ props.title}</h1>
+      <span className="stats">Players: { props.totalPlayers }</span>
+    </header>
+  );
+}
+
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        Guil
+      </span>
+    </div>
+  );
+}
+
+const Counter = () => {
+  return (
+    <button className="counter-action decrement"> - </button>
+    // <span className="counter-score">35</span>
+    <button className="counter-action increment"> + </button>
+  );
+}
+
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header
+      title="Scoreboard"
+      totalPlayers={1}
+     />
+
+      <Player />
+  );
+}
+
 
 ReactDOM.render(
-  <Header />,
+  <App />,
   document.getElementById('root')
 );
