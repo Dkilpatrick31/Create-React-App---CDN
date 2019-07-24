ea0dac25328deb06a7ae7e400f46df5a00ae3c9f
Dkilpatrick31 <dkilpatrick1990@gmail.com>
2019-07-24 01:03:43 -0500

    I wrote the Header React Component with an arrow function.

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-const desc = 'I just learned how to create a React node and render it into the DOM.';
-const myTitleID = 'main-title';
-const name = 'Guil';
-
-const header = (
-  <header>
-    <h1 id={myTitleID}>{ name }'s First React Element!</h1>
-    <p>{ desc }</p>
-    <input value={5443 * 3} />
-  </header>
+const Header = () => (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
 );
 
+
 ReactDOM.render(
-  header,
+  <Header />,
   document.getElementById('root')
 );
